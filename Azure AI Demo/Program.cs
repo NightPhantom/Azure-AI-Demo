f04905f46ecf1e9aaddf232e7c4d47f0d4affb62
Andy C <theandrescosta@gmail.com>
2025-01-09 23:03:51 -0500

    Configuring Azure AI Language service.

--- Azure AI Demo/Program.cs
+++ Azure AI Demo/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.Extensions.Configuration;
+
 namespace Azure_AI_Demo
 {
     internal static class Program
     {
+        public static IConfiguration Configuration { get; private set; }
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
@@ -11,6 +15,9 @@ namespace Azure_AI_Demo
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+            Configuration = new ConfigurationBuilder()
+                .AddUserSecrets<frmMain>()
+                .Build();
             Application.Run(new frmMain());
         }
     }
--- Azure AI Demo/frmMain.cs
+++ Azure AI Demo/frmMain.cs
@@ -1,13 +1,28 @@
+
+using Azure;
+using Azure.AI.TextAnalytics;
+using Microsoft.Extensions.Configuration;
+
 namespace Azure_AI_Demo
 {
     public partial class frmMain : Form
     {
-        private string _languageEndpoint;
-        private string _languageApiKey;
+        private TextAnalyticsClient? _languageClient;
 
         public frmMain()
         {
             InitializeComponent();
+            LoadConfiguration();
+        }
+
+        private void LoadConfiguration()
+        {
+            // Azure AI Language service
+            var languageEndpoint = Program.Configuration["AILanguageServiceEndpoint"];
+            var languageKey = Program.Configuration["AILanguageServiceKey"];
+            var languageUri = new Uri(languageEndpoint);
+            var languageCredentials = new AzureKeyCredential(languageKey);
+            _languageClient = new TextAnalyticsClient(languageUri, languageCredentials);
         }
     }
 }
